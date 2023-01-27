a0dd92194b3eb70ddcda94b693f89a98f4d604ac
vlasnata80@gmail.com <vlasnata80@gmail.com>
2023-01-27 14:04:55 +0200

    add stopwatch

--- LockExample/FileService.cs
+++ LockExample/FileService.cs
@@ -1,4 +1,6 @@
-﻿namespace LockExample
+﻿using System.Diagnostics;
+
+namespace LockExample
 {
     internal static class FileService
     {
@@ -19,23 +21,32 @@
         }
         public static async Task CopyFileAsync(string source, string destination)
         {
+            var stopWatch = new Stopwatch();
             var fileEntries = Directory.GetFiles(source);
             var len = fileEntries.Length;
-            var tasks = new Task[len - 1];
+            var tasks = new Task[len];
             for (var i = 0; i < len; i++)
             {
-                Console.WriteLine(Path.GetFileName(fileEntries[i]));
-                tasks[i] = Task.Run(() => CopyToDestination(source, destination, fileEntries[i]));
+                lock (fileLock)
+                {
+                    stopWatch.Start();
+                    Console.WriteLine(Path.GetFileName(fileEntries[i]));
+                    tasks[i] = Task.Run(() => CopyToDestination(source, destination, fileEntries[i]));
+                    stopWatch.Stop();
+                    TimeSpan ts = stopWatch.Elapsed;
+                    Console.WriteLine($"RunTime {ts.TotalSeconds * 1000}");
+                }
+                Thread.Sleep(2000);
+
             }
             await Task.WhenAll(tasks).ConfigureAwait(false);
         }
+
         public static void CopyToDestination(string source, string destination, string namefile)
         {
-            lock (fileLock)
-            {
-                var currentFileName = Path.GetFileName(namefile);
-                File.Copy(Path.Combine(source, currentFileName), Path.Combine(destination, currentFileName), true);
-            }
+            var currentFileName = Path.GetFileName(namefile);
+            File.Copy(Path.Combine(source, currentFileName), Path.Combine(destination, currentFileName), true);
+
         }
     }
 }
